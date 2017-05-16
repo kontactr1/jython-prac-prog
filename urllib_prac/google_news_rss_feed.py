6f2245e5e07c2542b50dd4403df7e8a21de571e0
kontactr <gandhiyash1381997@gmail.com>
2017-05-16 19:02:42 +0530

    news search 1.0

--- urllib_prac/google_news_rss_feed.py
+++ urllib_prac/google_news_rss_feed.py
@@ -9,6 +9,16 @@ resp = ur.urlopen(req)
 xml = bs(resp,"xml",exclude_encodings="utf-8")
 
 for item in xml.findAll("link"):
-    print (item.text)
+    try:
+        print (item.text)
+        req1 = ur.Request(item.text,headers={"User-Agent":""})
+        news = ur.urlopen(req1)
+        page = bs(news,"html5lib")
+
+
+        for n in page.findAll("p"):
+            print (n.text)
+    except Exception as e:
+        print (e)
 
 print("end")
