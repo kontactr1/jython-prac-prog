ecb7341325d4c2124cd9a319b8041e8c8a2a51b9
kontactr <gandhiyash1381997@gmail.com>
2017-06-04 23:45:56 +0530

    send rec message methods added

--- flask_example/flask_message/DatabaseSetup.py
+++ flask_example/flask_message/DatabaseSetup.py
@@ -3,15 +3,16 @@ from datetime import datetime
 
 
 users = sqlite3.connect("users_data")
-users.execute("INSERT INTO user1 values('hellloooo','user3','"+str(datetime.now())+"')")
+"""users.execute("INSERT INTO user1 values('hellloooo','user3','"+str(datetime.now())+"')")
 users.execute("INSERT INTO user1 values('bye','user3','"+str(datetime.now())+"')")
 users.execute("INSERT INTO user1 values('bye','user3','"+str(datetime.now())+"')")
 users.execute("INSERT INTO user1 values('bye','user3','"+str(datetime.now())+"')")
 users.execute("INSERT INTO user1 values('hellloooo','user3','"+str(datetime.now())+"')")
 users.execute("INSERT INTO user1 values('hellloooo','user3','"+str(datetime.now())+"')")
 users.execute("INSERT INTO user1 values('hellloooo','user3','"+str(datetime.now())+"')")
-users.execute("INSERT INTO user2 values('hellloooo','user3','"+str(datetime.now())+"')")
-for item in users.execute("SELECT * FROM user1 where sender = 'user3'").fetchall():
+users.execute("INSERT INTO user2 values('hellloooo','user3','"+str(datetime.now())+"')")"""
+users.commit()
+for item in users.execute("SELECT * FROM user3 ").fetchall():
     print (item)
 print ("done")
 
--- flask_example/flask_message/route.py
+++ flask_example/flask_message/route.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-
+from datetime import datetime
 import redis
 from flask import render_template
 from flask import request
@@ -11,11 +11,13 @@ from __init__ import app
 
 DATABASE = "users_data"
 
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -24,17 +26,16 @@ def close_connection(exception):
         db.close()
 
 
-
-#------------------------------------------
-#database - users
-#username - email  -d                      |
-#username - password  -d
-#username  - friends -d
-#username  - requ       -d |
-#message:-                                |
-#user table:-                             |
+# ------------------------------------------
+# database - users
+# username - email  -d                      |
+# username - password  -d
+# username  - friends -d
+# username  - requ       -d |
+# message:-                                |
+# user table:-                             |
 # sender - message - time                 |
-#------------------------------------------
+# ------------------------------------------
 
 
 # username_session = None
@@ -43,7 +44,6 @@ user_pass = redis.StrictRedis(db=5, charset="utf-8", decode_responses=True)
 user_email = redis.StrictRedis(db=6, charset="utf-8", decode_responses=True)
 user_friends = redis.StrictRedis(db=7, charset="utf-8", decode_responses=True)
 user_req = redis.StrictRedis(db=8, charset="utf-8", decode_responses=True)
-
 
 
 @app.route("/", methods=["GET"])
@@ -151,7 +151,7 @@ def register():
 
 @app.route("/register/validate", methods=["GET", "POST"])
 def next_step():
-    users_msg = sqlite3.connect("users_data", check_same_thread=False,timeout=10.0)
+    # users_msg = sqlite3.connect("users_data", check_same_thread=False,timeout=10.0)
     if request.environ['REMOTE_ADDR'] + 'username' in session:
         return render_template("Dashboard.html", name=session[request.environ['REMOTE_ADDR'] + 'username'])
     else:
@@ -174,19 +174,24 @@ def next_step():
                 if session[request.environ['REMOTE_ADDR'] + '_' + 'code'] == request.form["code"]:
                     session.pop(request.environ['REMOTE_ADDR'] + '_' + 'code')
                     user_pass.set(session[request.environ['REMOTE_ADDR'] + 'temp_session_user'],
-                               session[request.environ['REMOTE_ADDR'] + 'temp_session_password'])
+                                  session[request.environ['REMOTE_ADDR'] + 'temp_session_password'])
                     user_email.set(session[request.environ['REMOTE_ADDR'] + 'temp_session_user'],
-                               session[request.environ['REMOTE_ADDR'] + 'temp_session_email'],
-                               )
+                                   session[request.environ['REMOTE_ADDR'] + 'temp_session_email'],
+                                   )
                     session.pop(request.environ['REMOTE_ADDR'] + 'temp_session_password')
                     #                    os.mkdir("Data\\"+session[request.environ['REMOTE_ADDR']+'temp_session_user'])
-                    user_friends.set(session[request.environ['REMOTE_ADDR'] + 'temp_session_user'],"")
+                    user_friends.set(session[request.environ['REMOTE_ADDR'] + 'temp_session_user'], "")
                     user_req.set(session[request.environ['REMOTE_ADDR'] + 'temp_session_user'], "")
                     get_conn = get_db()
                     get_cur = get_conn.cursor()
-                    get_cur.execute("CREATE TABLE "+session[request.environ['REMOTE_ADDR'] + 'temp_session_user']+"(message text, sender text, time date)")
+                    get_cur.execute("CREATE TABLE " + session[request.environ[
+                                                                  'REMOTE_ADDR'] + 'temp_session_user'] + "(message "
+                                                                                                          "text, "
+                                                                                                          "sender "
+                                                                                                          "text, "
+                                                                                                          "time date)")
                     get_conn.commit()
-                    get_conn.close()
+                    close_connection(Exception)
                     session.pop(request.environ['REMOTE_ADDR'] + 'temp_session_user')
                     session.pop(request.environ['REMOTE_ADDR'] + 'temp_session_email')
 
@@ -219,18 +224,19 @@ def next_step():
             #    return render_template("Dashboard.html",name=session[request.environ['REMOTE_ADDR']+'username'])
 
 
-@app.route("/<title>",methods=["GET","POST"])
+@app.route("/<title>", methods=["GET", "POST"])
 def find_Friend(title):
     if request.method == "GET":
         if title == 'Add Friends':
-            return render_template("AddFriend.html",storage_di = [],val="",title="")
+            return render_template("AddFriend.html", storage_di=[], val="", title="")
         elif title == 'Req Friends':
             get_req = user_req.get(session[request.environ['REMOTE_ADDR'] + 'username']).strip(",").split(",")
             if "" in get_req:
                 get_req.remove("")
-            return render_template("ReqFriend.html",storage_di=get_req,title="Req for you: ")
-        elif title  == "show Friends":
-            return render_template("ShowFriends.html",storage_di=user_friends.get(session[request.environ['REMOTE_ADDR'] + 'username']).strip(",").split(","))
+            return render_template("ReqFriend.html", storage_di=get_req, title="Req for you: ")
+        elif title == "show Friends":
+            return render_template("ShowFriends.html", storage_di=user_friends.get(
+                session[request.environ['REMOTE_ADDR'] + 'username']).strip(",").split(","))
         else:
             return str(None)
     elif request.method == "POST":
@@ -238,26 +244,27 @@ def find_Friend(title):
             search_text = request.form["search_item"]
             add_friend_list = request.form.getlist("ad_fr_list")
             storage_di = dict()
-            if(len(add_friend_list) > 0):
+            if len(add_friend_list) > 0:
                 for friend_list in add_friend_list:
-                    user_req.append(friend_list,session[request.environ['REMOTE_ADDR'] + 'username']+",")
+                    user_req.append(friend_list, session[request.environ['REMOTE_ADDR'] + 'username'] + ",")
             temp_user = session[request.environ['REMOTE_ADDR'] + 'username']
-            for friend_u in user_pass.keys(pattern=search_text+"*"):
-                if temp_user not in user_req.get(friend_u ) \
-                    and friend_u not in user_req.get(temp_user) \
-                    and  friend_u not in user_friends.get(temp_user):
+            for friend_u in user_pass.keys(pattern=search_text + "*"):
+                if temp_user not in user_req.get(friend_u) \
+                        and friend_u not in user_req.get(temp_user) \
+                        and friend_u not in user_friends.get(temp_user):
                     storage_di[friend_u] = "Send Request"
-            del storage_di[session[request.environ['REMOTE_ADDR'] + 'username']]
-            return render_template("AddFriend.html", storage_di=storage_di,val=search_text,title="Add Friend:- ")
+            if session[request.environ['REMOTE_ADDR'] + 'username'] in storage_di:
+                del storage_di[session[request.environ['REMOTE_ADDR'] + 'username']]
+            return render_template("AddFriend.html", storage_di=storage_di, val=search_text, title="Add Friend:- ")
         elif title == 'Req Friends':
             req_list = user_req.get(session[request.environ['REMOTE_ADDR'] + 'username']).strip(",").split(",")
             temp_del_list = []
 
             for item in req_list:
-                li_fri = request.form["rad_li_"+item]
+                li_fri = request.form["rad_li_" + item]
                 if li_fri == "Acc":
-                    user_friends.append(session[request.environ['REMOTE_ADDR'] + 'username'],item+",")
-                    user_friends.append(item,session[request.environ['REMOTE_ADDR'] + 'username']+ ",")
+                    user_friends.append(session[request.environ['REMOTE_ADDR'] + 'username'], item + ",")
+                    user_friends.append(item, session[request.environ['REMOTE_ADDR'] + 'username'] + ",")
                     temp_del_list.append(item)
                 elif li_fri == "Rej":
                     temp_del_list.append(item)
@@ -265,17 +272,14 @@ def find_Friend(title):
                     pass
             for item in temp_del_list:
                 req_list.remove(item)
-            #map(lambda x:req_list.remove(x),temp_del_list)
-            user_req.set(session[request.environ['REMOTE_ADDR'] + 'username'],",".join(req_list) + ",")
-            #return str(True)
+            # map(lambda x:req_list.remove(x),temp_del_list)
+            user_req.set(session[request.environ['REMOTE_ADDR'] + 'username'], ",".join(req_list) + ",")
+            # return str(True)
             if "" in req_list:
                 req_list.remove("")
-            return render_template("ReqFriend.html",storage_di=req_list,title="Req for you: ")
+            return render_template("ReqFriend.html", storage_di=req_list, title="Req for you: ")
     else:
         return render_template("ImageDisplay.html")
-
-
-
 
 
 @app.route("/logout", methods=["GET", "POST"])
@@ -295,9 +299,6 @@ def logout():
         return render_template("Login.html")
 
 
-
-
-
 def convert_bytes(num):
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
@@ -310,41 +311,53 @@ def cal_size(file_name):
         file_info = os.stat(file_name)
         return convert_bytes(file_info.st_size)
 
-@app.route("/ChatFriend",methods=["GET","POST"])
-def ChatFriend():
 
+@app.route("/ChatFriend", methods=["GET"])
+def ChatFriend():
     if request.method == "GET":
-        #user_msg = sqlite3.connect("users_data",check_same_thread=False,timeout=10.0)
         db_c = get_db()
         db_cu = db_c.cursor()
-        #return str(db_cu.__str__()+"hello world")
+
         user_name = (request.args['button'])[10:]
-        #msg_dr = users_msg.cursor()
-        #msg_dr_c = msg_dr.execute("SELECT * FROM user1")
-        from datetime import datetime
-        l = []
-        for data in db_cu.execute("SELECT * FROM user1").fetchall():
-            return "True"
-            l.append("True")
-        return str(l)
+        data1 = db_cu.execute("SELECT * FROM " + session[
+            request.environ['REMOTE_ADDR'] + 'username'] + " WHERE sender = '" + user_name + "'")
 
-        return "Done"
+        data1 = data1.fetchall()
+        close_connection(Exception)
+        assert isinstance(user_name, str)
         return render_template("Messages.html",
-
-                               title = user_name
-
+                               msg=data1,
+                               title=user_name
                                )
 
 
+@app.route("/messages<title>", methods=["GET", "POST"])
+def messages(title):
+    date = datetime.now()
+    db_c = get_db()
+    db_cu = db_c.cursor()
+    data1 = db_cu.execute("SELECT * FROM " + session[
+        request.environ['REMOTE_ADDR'] + 'username'] + " WHERE sender = '" + title + "'")
+    data1 = data1.fetchall()
+    if request.method == "GET":
 
+        close_connection(Exception)
+    elif request.method == "POST":
 
+        text_area_resp = request.form["text_area"]
+        db_cu.execute("INSERT INTO " + title + " VALUES('" + text_area_resp + "','" + session[
+            request.environ['REMOTE_ADDR'] + 'username'] + "','" + str(date) + "')")
+        db_c.commit()
+        close_connection(Exception)
+    return render_template("Messages.html",
+                           msg=data1,
+                           title=title
+                           )
 
 
 @app.errorhandler(404)
 def page_not_found():
     return render_template("ImageDisplay.html", name="404-snake.png"), 404
-
-
 
 
 @app.route("/test", methods=["GET", "POST"])
